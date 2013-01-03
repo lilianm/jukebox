9824d33b1e425a5ffb4857e54a65b10843291582
Lilian Maurel <lilian@maurel.biz>
2013-01-04 00:11:52 +0100

    [jukebox] replace quick HTTP server implementation by new implementation

--- jukebox.c
+++ jukebox.c
@@ -16,37 +16,26 @@ static char basic_reponse[] =
     "Connection: Close\r\n"
     "\r\n";
 
-static void on_http_data(io_event_t *ev, int sck, void *data)
+static int on_stream(http_request_t *hr, void *data, const char *remaining, size_t size)
 {
-    char buffer[1024];
-    int  ret;
+    int sck;
 
+    (void) remaining;
+    (void) size;
     (void) data;
 
-    ret = recv(sck, buffer, sizeof(buffer), 0);
-    if(ret != 0 && ret != -1) {
-        send(sck, basic_reponse, sizeof(basic_reponse) - 1, MSG_DONTWAIT);
-        channel_create(sck);
-    }
-    event_delete(ev);
-}
+    sck = http_request_detach(hr);
 
-static void on_http_server(io_event_t *ev, int sck,
-                           struct sockaddr_in *addr, void *data)
-{
-    io_event_t *client;
-
-    (void) ev;
-    (void) addr;
-    (void) data;
-
-    client = event_client_add(sck, NULL);
-    event_client_set_on_read(client, on_http_data);
+    send(sck, basic_reponse, sizeof(basic_reponse) - 1, MSG_DONTWAIT);
+    channel_create(sck);
+ 
+    return 0;
 }
 
 int main(int argc, char *argv[])
 {
-    int port = 8080;
+    int            port           = 8080;
+    http_server_t *server;
 
     (void) argc;
     (void) argv;
@@ -56,12 +45,8 @@ int main(int argc, char *argv[])
     encoder_scan();
     event_init();
 
-    if(event_server_add(port, on_http_server, NULL) == NULL) {
-        print_error("Can't listen port %i: %m", port);
-        return -1;
-    }
-
-    http_server_new(8084, NULL);
+    server = http_server_new(port);
+    http_node_new(server, "/stream", on_stream, NULL);
 
     print_log("Jukebox started");
 
