fa5a75e891a33b09dc0e686036bf705eb8889c49
Lilian Maurel <lilian@maurel.biz>
2013-02-01 01:22:50 +0100

    [jukebox] add next fir url /api/basic/next

--- jukebox.rb
+++ jukebox.rb
@@ -3,10 +3,60 @@ $:.unshift File.dirname($0)
 
 require 'ffi'
 
-module Jukebox
+module Jukebox_wrapping
   extend FFI::Library
   ffi_lib "./jukebox.so"
 
   attach_function :jukebox_init, [:int], :pointer
   attach_function :jukebox_launch, [], :void
+
+  callback :request_cb, [:pointer, :pointer, :string, :int], :int
+
+  attach_function :http_send_reponse_and_dup, [:pointer, :string, :string, :int], :void
+  attach_function :http_node_new, [:pointer, :string, :request_cb, :pointer], :void
+
+  attach_function :http_request_get_data, [:pointer], :pointer
+
+  attach_function :user_get_channel, [:pointer], :pointer
+
+  attach_function :channel_next, [:pointer], :int
+end
+
+
+module Jukebox
+  def init(port)
+    Jukebox_wrapping::jukebox_init(port)
+  end
+
+  def launch()
+    Jukebox_wrapping::jukebox_launch();
+  end
+
+  class Channel
+    def initialize(user_hdl)
+      @hdl = Jukebox_wrapping::user_get_channel(user_hdl);
+    end
+
+    def next()
+      Jukebox_wrapping::channel_next(@hdl);
+    end
+  end
+
+  module Http
+    def send_reponse(hdl, content_type = nil, reponse)
+      content_type ||= "text/html"
+
+      Jukebox_wrapping::http_send_reponse_and_dup(hdl, content_type, reponse, reponse.size);
+    end
+
+    def node_new(hdl, path,  &block)
+      callback = Proc.new { |hdl,data,remaining,size|
+        user_hdl = Jukebox_wrapping::http_request_get_data(hdl);
+        channel  = Channel.new(user_hdl);
+        rem      = remaining[0...size];
+        block.call(hdl, data, rem, channel);
+      }
+      Jukebox_wrapping::http_node_new(hdl, path, callback, nil);
+    end
+  end
 end
--- main.rb
+++ main.rb
@@ -2,6 +2,16 @@
 $:.unshift File.dirname($0)
 
 require 'jukebox'
+include Jukebox
+include Jukebox::Http
 
-Jukebox::jukebox_init(8085);
-Jukebox::jukebox_launch();
+http_server = Jukebox::init(8085);
+
+Jukebox::Http::node_new(http_server, "/api/basic/next") { |hdl,data,remaining, channel|
+  channel.next();
+  page =  "<head><title>Next</title></head><body><h1>Next</h1></body>"
+  Jukebox::Http::send_reponse(hdl, "text/html", page)
+  0
+}
+
+Jukebox::launch();
